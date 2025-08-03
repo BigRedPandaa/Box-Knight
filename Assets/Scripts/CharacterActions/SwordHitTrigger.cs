b4b2290057a45ab6940034de7e6f635bc8752185
BigRedPandaa <146365964+BigRedPandaa@users.noreply.github.com>
2025-08-03 02:13:02 -0400

    ADDED COLLSIONS AND TEXUTRES TO THE HOUSE

--- Assets/Scripts/CharacterActions/SwordHitTrigger.cs
+++ Assets/Scripts/CharacterActions/SwordHitTrigger.cs
@@ -1,15 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class TriggerDelete : MonoBehaviour
+public class SwordHitTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Hit: " + other.name);
-
         // Example: check if it's an enemy
         if (other.CompareTag("Enemy"))
         {
             Debug.Log("Enemy Hit");
+            Destroy(other.gameObject);
         }
     }
 }
+
